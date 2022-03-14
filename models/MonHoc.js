a7f5281cb15a52bf364e44120bdd2591705f63f2
Marcus Nguyen <immahoon@gmail.com>
2022-03-14 21:08:08 +0700

    devA_code_monhoc.js

---
+++ models/MonHoc.js
@@ -0,0 +1,7 @@
+function MonHoc() {
+    this.maMonHoc = '';
+    this.tenMonHoc = '';
+    this.hienThiThongTinMonHoc = function () {
+        console.log('Thong tin mon hoc', this.maMonHoc, this.tenMonHoc);
+    }
+}
